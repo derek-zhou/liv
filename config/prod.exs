d99c54d18b17c6fe3a7e6bc4645c6e67b5a64964
Derek Zhou <derek@3qin.us>
2021-04-22 03:12:02 +0000

    add missing config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :liv, LivWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "mail.3qin.us", scheme: "https", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
