c36033ee0a4e7101a7d86be2331b8ec0cf443a1e
Derek Zhou <derek@3qin.us>
2021-08-15 20:23:11 +0000

    use runtime config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,11 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 # Configures the endpoint
 config :liv, LivWeb.Endpoint,
@@ -42,4 +42,4 @@ config :argon2_elixir,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
---
+++ config/runtime.exs
@@ -0,0 +1,35 @@
+import Config
+
+if config_env() == "prod" do
+  # In this file, we load production configuration and secrets
+  # from environment variables. You can also hardcode secrets,
+  # although such is generally not recommended and you have to
+  # remember to add this file to your .gitignore.
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  guardian_key =
+    System.get_env("GUARDIAN_KEY") ||
+      raise """
+      environment variable GUARDIAN_KEY is missing.
+      You can generate one by calling: mix guardian.gen.secret
+      """
+
+  config :liv, LivWeb.Endpoint,
+    url: [host: "mail.3qin.us", scheme: "https", port: 443],
+    cache_static_manifest: "priv/static/cache_manifest.json",
+    http: [
+      ip: {127, 0, 0, 1},
+      port: String.to_integer(System.get_env("PORT") || "4001")
+    ],
+    secret_key_base: secret_key_base,
+    server: true
+
+  # for guardian
+  config :liv, LivWeb.Guardian, secret_key: guardian_key
+end
