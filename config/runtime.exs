ec6227c1eca06faa78ee9ef1a2fbd6ad4e88bc39
Derek Zhou <derek@3qin.us>
2021-08-15 20:32:31 +0000

    fix silly bug

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
-if config_env() == "prod" do
+if config_env() == :prod do
   # In this file, we load production configuration and secrets
   # from environment variables. You can also hardcode secrets,
   # although such is generally not recommended and you have to
