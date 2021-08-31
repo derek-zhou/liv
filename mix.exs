f834e38eae0083a4fcc80eedd9b53107f2707de9
Derek Zhou <derek@3qin.us>
2021-08-31 15:38:51 +0000

    Now I am using runtime.exs I need to bump the elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Liv.MixProject do
     [
       app: :liv,
       version: "0.2.2",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
