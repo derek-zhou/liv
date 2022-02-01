746d87a90e19c640c151cf6082b117ed712353b5
Derek Zhou <derek@3qin.us>
2022-02-01 14:30:34 -0500

    upgrade snowpack and tailwind

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
+	require('tailwindcss/nesting'),
         require('tailwindcss'),
-        require('postcss-nested'),
-        require('autoprefixer'),
+	require('autoprefixer'),
     ],
 };
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    purge: {
-        enabled: true,
-        layers: ['utilities'],
-        content: [],
-    },
     theme: {
-        screens: {
+	screens: {
             sm: '512px',
             md: '768px',
             lg: '1024px',
             xl: '1280px',
-        },
-        extend: {},
+	},
     },
-    variants: {},
-    plugins: [],
 };
